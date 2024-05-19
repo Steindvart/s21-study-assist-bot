b79f4b3d795be955249fc911d26e9a11cd88d3b4
Ivan Kalashnikov <ivank.public@gmail.com>
2024-05-19 12:36:21 +0700

    (ref) - types specification, @dataclass for `Section` class.

--- src/section.py
+++ src/section.py
@@ -1,20 +1,24 @@
+from dataclasses import dataclass
+from typing import List
+
 import logging
 import os
 
+@dataclass
 class Section:
-  def __init__(self, name):
-    self.name = name
+  name: str
+  path: str
 
-  def __repr__(self):
+  def __repr__(self) -> str:
     return self.name
 
-def get_sections(directory="../content"):
+def get_sections(directory="../content") -> List[Section]:
   sections = []
   try:
     for entry in os.listdir(directory):
       entry_path = os.path.join(directory, entry)
       if os.path.isdir(entry_path):
-        sections.append(Section(entry))
+        sections.append(Section(entry, entry_path))
   except Exception as e:
     logging.error(f"Error accessing directory {directory}: {e}")
 
